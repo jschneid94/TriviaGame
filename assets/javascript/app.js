f71ae4f3444e671118ea1d4b6b6db24f1103028a
SchneiderMan <jschneid94@gmail.com>
2018-07-03 15:50:21 -0700

    Restarted to create questions and game functions outside of object.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,108 +1,39 @@
+// Object array for each question and question values
+var questions = [
+    { question: "",
+      choices: ["", "", "", ""];
+      answer: ,
+    }, 
+    { question: "",
+      choices: ["", "", "", ""];
+      answer: ,
+
+    }
+];
+
+// GLOBAL VARIABLES
+// Count for correct, incorrect, and unanswered questions
+var correct = 0;
+var incorrect = 0
+var unAnswered = 0;
+// Timer count
+var timer = 20;
+// Variable for setting interval
+var intervalId;
+// Boolean for if timer is on
+var timerOn = false;
+// Variable to store user guess
+var userGuess;
+
+// FUNCTIONS
+
 // On document ready...
 $(document).ready(function() {
 
-    // Hide the timer
-    $("#timeRemaining").hide();
-    // Start game when button is clicked
-    $("#start").on("click", trivia.startGame);
-    // Check the answer when an option is clicked on the screen
-    $(document).on("click", ".option", trivia.checkGuess);
+    // Only display start game button on to screen
 
-    var trivia = {
-        // Trivia properties
-        correctAnswers = 0,
-        incorrectAnswers = 0,
-        noAnswers = 0,
-        currentSet = 0,
-        timer = 20,
-        timerOn = false,
-        timerId = '',
-         // questions options and answers data
-        questions = {
-            q1: "",
-            q2: "",
-            q3: "",
-            q4: "",
-            q5: "",
-            q6: "",
-            q7: "",
-            q8: "",
-            q9: "",
-            q10: ""
-        },
-        options = {
-            q1: ["", "", "", ""],
-            q2: ["", "", "", ""],
-            q3: ["", "", "", ""],
-            q4: ["", "", "", ""],
-            q5: ["", "", "", ""],
-            q6: ["", "", "", ""],
-            q7: ["", "", "", ""],
-            q8: ["", "", "", ""],
-            q9: ["", "", "", ""],
-            q10: ["", "", "", ""]
-        },
-        answers = {
-            q1: "",
-            q2: "",
-            q3: "",
-            q4: "",
-            q5: "",
-            q6: "",
-            q7: "",
-            q8: "",
-            q9: "",
-            q10: ""
-        },
-        
-        // METHODS
-
-        // Initializes the game
-        startGame: function() {
-            trivia.correctAnswers = 0;
-            trivia.incorrectAnswers = 0;
-            trivia.noAnswers = 0;
-            trivia.currentSet = 0;
-
-            // Display the game section
-            $("#gameSection").show();
-
-            // Remove game results from the page
-            $("#gameResults").hide();
-
-            // Display the timer
-            $("#timeRemaining").show();
-            $("#timer").text(trivia.timer);
-
-            // Hide the start button
-            $("#start").hide();
-
-            // Ask the first question
-            trivia.askQuestion()
-        },
-        
-        askQuestion: function() {
-            // Reset the timer
-            trivia.timer = 20;
-            $("#timer").text(trivia.timer);
-
-            // Get the question value from the object and display to DOM
-            var question = Object.values(trivia.questions)[trivia.currentSet];
-            $("#triviaQuestion").text(question);
-
-            // Grab the array of all possible answers for the chosen answer and generate to the DOM
-            var questionOptions = Object.values(trivia.options)[trivia.currentSet];
-            $.each(questionOptions, function(key) {
-                $("options").append("<button>" + key + "</button>");
-            });
+    // When user clicks start button, game starts
 
 
-        },
-
-        checkGuess: function() {
-
-        },
-
-    };
 
 });
