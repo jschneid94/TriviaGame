47884023eafa14569c8c398346debfd9ab1ccf6c
SchneiderMan <jschneid94@gmail.com>
2018-07-03 16:50:05 -0700

    Started pseudo code for my functions.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -27,6 +27,60 @@ var userGuess;
 
 // FUNCTIONS
 
+// Displays the timer and the question area, hides other sections
+function startGame() {
+    // Show timer
+    // Show question area
+    // Hide start button
+    // run askQuestion
+    askQuestion();
+    // run startTimer
+    startTimer();
+}
+
+// Starts the interval on the timer
+function startTimer() {
+    if (!timerOn) {
+        intervalId = setInterval(decrementTimer, 1000);
+        timerOn = true;
+    }
+}
+
+// Decrements the timer
+function decrementTimer() {
+    // Display the count on the screen
+    // Decrement the timer by 1.
+
+    // If timer reaches zero...
+        unAnswered++;
+        stopTimer();
+
+}
+
+// Stops the timer
+function stopTimer() {
+
+}
+
+// Displays a new question onto the DOM
+function askQuestion() {
+    // Generate a random question from questions object
+    // Display that object information to the DOM
+}
+
+// Checks if the user's guess is correct
+function checkGuess() {
+    
+}
+
+// Displays the correct answer for a few seconds
+function displayAnswer() {
+    // Display the correct answer
+        // setTimeout();
+    // Push question index into another array so it won't be chosen again
+    askQuestion();
+}
+
 // On document ready...
 $(document).ready(function() {
 
