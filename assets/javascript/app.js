2554c568ee585e73158db5babe95324658f5d0eb
SchneiderMan <jschneid94@gmail.com>
2018-07-02 21:26:45 -0700

    Added empty objects and started methods.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,15 +4,15 @@ $(document).ready(function() {
     // Hide the timer
     $("#timeRemaining").hide();
     // Start game when button is clicked
-    $("#start").on("click", );
+    $("#start").on("click", trivia.startGame);
     // Check the answer when an option is clicked on the screen
-    $(document).on("click", ".option", );
+    $(document).on("click", ".option", trivia.checkGuess);
 
     var trivia = {
         // Trivia properties
-        correct = 0,
-        incorrect = 0,
-        noAnswer = 0,
+        correctAnswers = 0,
+        incorrectAnswers = 0,
+        noAnswers = 0,
         currentSet = 0,
         timer = 20,
         timerOn = false,
@@ -54,6 +54,45 @@ $(document).ready(function() {
             q9: "",
             q10: ""
         },
+        
+        // METHODS
+
+        // Initializes the game
+        startGame: function() {
+            trivia.correctAnswers = 0;
+            trivia.incorrectAnswers = 0;
+            trivia.noAnswers = 0;
+            trivia.currentSet = 0;
+
+            // Display the game section
+            $("#gameSection").show();
+
+            // Remove game results from the page
+            $("#gameResults").hide();
+
+            // Display the timer
+            $("#timeRemaining").show();
+            $("#timer").text(trivia.timer);
+
+            // Hide the start button
+            $("#start").hide();
+
+            // Ask the first question
+            trivia.askQuestion()
+        },
+        
+        askQuestion: function() {
+            // Reset the timer
+            trivia.timer = 20;
+            $("#timer").text(trivia.timer);
+
+            
+        },
+
+        checkGuess: function() {
+
+        },
+
     };
 
 });
